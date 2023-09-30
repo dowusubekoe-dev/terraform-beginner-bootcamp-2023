2996a92eeaa80fc04ae162ed1a05cf5d731105df
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2023-09-30 00:33:41 +0000

    # 12 resolve the terraform destroy error

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-
-  cloud {
-    organization = "dowusubekoe-dev"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -19,26 +12,22 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-}
-# provider "random" {
   # Configuration options
-# }
+}
+provider "random" {
+  # Configuration options
+}
 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  lower   = true
-  upper   = false
+  lower = true
+  upper = false
   length  = 32
   special = false
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
-
   # Bucket Naming Rules
-
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 }
 
