a251cf6bd6bc6fd3f4aae7f29eefe34df572c4e7
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2023-09-29 20:41:09 -0400

    12 simple s3 bucket (#26)
    * #12 install terraform provider, create s3 bucket
    
    * #12 resolve aws permissions and iam roles.
    
    * changed aws keys due to no valid credential sources found error
    
    * resolve terraform version issue.
    
    * removed aws region
    
    * #12 removed generate_tfrc and set_tf_alias
    
    * # 12 resolve the terraform destroy error

--- main.tf
+++ main.tf
@@ -7,13 +7,24 @@ terraform {
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-  length   = 16
-  special  = false
+  lower = true
+  upper = false
+  length  = 32
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
