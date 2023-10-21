cffc91c23e403a74174e44321ebf9d1085427b36
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2023-10-20 22:25:35 -0400

    #32 only upload index.html if a content version has changed. (#58)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -28,8 +28,13 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
   content_type = "text/html"
   #source = "${path.root}/public/index.html"
+
   etag = filemd5(var.index_html_filepath)
   #etag = filemd5("${path.root}/public/index.html")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -41,6 +46,9 @@ resource "aws_s3_object" "error_html" {
   #source = "${path.root}/public/error.html"
   etag = filemd5(var.error_html_filepath)
   #etag = filemd5("${path.root}/public/error.html")
+  #lifecycle {
+  #  ignore_changes = [etag]
+  #}
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -65,4 +73,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
